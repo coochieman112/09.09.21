043813b681e99c4b92f867d5c320c82f65e2aea6
Martiss <78596983+coochieman112@users.noreply.github.com>
2021-09-09 09:26:16 +0300

    Add files via upload

---
+++ jaaaa.js
@@ -0,0 +1,33 @@
+<script>
+    var x = 3
+
+    var vards = "Mani sauc Martiss";
+
+    var atzimes = [1,2,3,4,5];
+
+    var teksta_masivs = ["Gulta", "Spilvens", "Sega"];
+
+    if ( x==2 ) {
+
+        alert("Es gribu gulēt")
+        }
+    else{
+
+        alert("Es tāpat gribu gulēt")
+        
+    }
+
+    for( var sk = 0; sk 10; sk++  ){
+
+    console.log(sk) // Konsolē tiks izvadīts skaitlis intervālā no 0 līdz 9
+
+}
+    var vertiba = document.getElementById("identifikators");
+
+   function yes(a + b) { 
+         a+b
+   }
+   saskaiti (1, 3); // atbilde 4
+        
+   &rbrace;
+</script>
